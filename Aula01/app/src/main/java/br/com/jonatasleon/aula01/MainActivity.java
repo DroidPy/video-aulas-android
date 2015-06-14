a22593be6ba219662faed6c010edf29884b34686
Jonatas Leon <jonatas.leon@gmail.com>
2015-06-13 23:23:56 -0300

    Add video aula 01 e 03

---
+++ Aula01/app/src/main/java/br/com/jonatasleon/aula01/MainActivity.java
@@ -0,0 +1,33 @@
+package br.com.jonatasleon.aula01;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by jonatasleon on 07/06/15.
+ */
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = (EditText) findViewById(R.id.editText);
+                String text = editText.getText().toString();
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText(text);
+            }
+        });
+    }
+}
---
+++ Aula03/app/src/main/java/br/com/jonatasleon/aula03/MainActivity.java
@@ -0,0 +1,17 @@
+package br.com.jonatasleon.aula03;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by jonatasleon on 13/06/15.
+ */
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+
+    }
+}
